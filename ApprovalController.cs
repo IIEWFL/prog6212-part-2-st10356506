a3903f2d5662bce179c8b658ae32a3d8f8e48a84
Yadav Priaram <131756403+st10356506@users.noreply.github.com>
2024-10-16 18:07:42 +0200

    commit 2
    created and updated controllers

---
+++ ApprovalController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMCS_Application.Models;
+
+namespace CMCS_Application.Controllers
+{
+    public class ApprovalController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(ClaimMemory.ClaimList);
+        }
+
+        [HttpPost]
+        public IActionResult Approve(int claimID)
+        {
+            var claim = ClaimMemory.ClaimList.FirstOrDefault(c => c.ClaimId == claimID);
+            if (claim != null)
+            {
+                claim.Status = ClaimStatus.Approved;
+                ClaimMemory.ClaimList.Remove(claim);
+                ClaimMemory.ClaimHistory.Add(claim);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Reject(int claimID)
+        {
+            var claim = ClaimMemory.ClaimList.FirstOrDefault(c => c.ClaimId == claimID);
+            if (claim != null)
+            {
+                claim.Status = ClaimStatus.Rejected;
+                ClaimMemory.ClaimList.Remove(claim);
+                ClaimMemory.ClaimHistory.Add(claim);
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
