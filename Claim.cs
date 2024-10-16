06b7c2c3affb93e4fd900e836d98ac0271895002
Yadav Priaram <131756403+st10356506@users.noreply.github.com>
2024-10-16 18:06:30 +0200

    commit 1
    created and populated models

---
+++ Claim.cs
@@ -0,0 +1,22 @@
+﻿using CMCS_Application.Models;
+
+namespace CMCS_Application.Models
+{
+    public class Claim
+    {
+    public int ClaimId { get; set; }
+    public string LecturerName { get; set;}
+    public string LecturerEmail { get; set;}
+    public int HoursWorked { get; set; }
+    public int HourlyRate { get; set; }
+    public string AdditionalNotes { get; set; }
+    public ClaimStatus Status { get; set; } = ClaimStatus.Pending;
+        public List<string> Documents { get; set; } = new List<string>();
+    }
+    public enum ClaimStatus
+    {
+        Pending,
+        Approved,
+        Rejected
+    }
+}
---
+++ ClaimMemory.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace CMCS_Application.Models
+{
+    public class ClaimMemory
+    {
+        public static List<Claim> ClaimList = new List<Claim>();
+        public static List<Claim> SubmittedClaim = new List<Claim>();
+        public static List<Claim> ClaimHistory = new List<Claim>();
+        }
+    }
+
